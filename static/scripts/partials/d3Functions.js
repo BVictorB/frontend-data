208ee4fdab1e7ca51810d1f53976bc230c2a3e17
bvictorb <victory-boucher@hotmail.com>
2020-11-05 10:49:15 +0100

    added readme

--- static/scripts/partials/d3Functions.js
+++ static/scripts/partials/d3Functions.js
@@ -1,5 +1,4 @@
 import { select, json, geoPath, geoMercator, zoom } from 'd3'
-import { feature } from 'topojson'
 
 const svg = select('svg')
 const projection = geoMercator().scale(50000).center([4.895168,52.370216])
@@ -9,12 +8,11 @@ const districts = svg.append('g')
 const amsterdamGeo = 'https://gist.githubusercontent.com/BVictorB/f0859579f276725bbdd27e01b8c73fab/raw/2d3caf5e08b24df850d79479201d518f505d4777/amsterdam.geojson'
 const amsterdamStadsdelen = 'https://maps.amsterdam.nl/open_geodata/geojson.php?KAARTLAAG=GEBIED_STADSDELEN&THEMA=gebiedsindeling'
 
-async function createMap() {
+const createMap = async () => {
     const geoData = await getData(amsterdamGeo)
     const stadsdelen = await getData(amsterdamStadsdelen)
-    console.log(geoData.features)
     const filteredGeoData = geoData.features.filter(e => e.geometry && e.geometry.type !== 'Point')
-    console.log(filteredGeoData)
+    
     svg.call(zoom().on('zoom', (e) => {
         main.attr('transform', e.transform)
         districts.attr('transform', e.transform)
@@ -37,7 +35,6 @@ async function createMap() {
                 .attr('fill', 'none')
                 .attr('stroke', '#2b9348')
                 .attr('stroke-width', '.1')
-
 }
 
 export const drawMap = (pointData) => {
@@ -50,7 +47,7 @@ export const drawMap = (pointData) => {
                 .text(d => d.properties.name)
 }
 
-async function getData(url) {
+const getData = async (url) => {
     let data = await json(url)
     return data
 }
